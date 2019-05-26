bcd6590c5d1319aa6d0da0a8bcb43bdc68d16c8c
Scott Bedard <scottbedard90@gmail.com>
2019-05-26 14:38:08 -0700

    pages slipped out of source control

---
+++ src/pages/About.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
+<h2 class="font-bold">About</h2>
+<p>I like to code</p>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>Sveltober</title>
+</svelte:head>
+
+<h2 class="font-bold">Home</h2>
+<p>For help getting started, <a class="text-red-400 hover:text-600" href="https://github.com/scottbedard/sveltober">documentation can be found here</a>.</p>
