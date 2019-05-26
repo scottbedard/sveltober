24f1d529262d0ffc5939d45c29569006e0561f3d
Scott Bedard <scottbedard90@gmail.com>
2019-05-26 14:44:46 -0700

    minor tweak

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -3,4 +3,4 @@
 </svelte:head>
 
 <h2 class="font-bold">Home</h2>
-<p>For help getting started, <a class="text-red-400 hover:text-600" href="https://github.com/scottbedard/sveltober">documentation can be found here</a>.</p>
+<p>For help getting started, <a class="text-red-400 hover:text-600" href="https://github.com/scottbedard/sveltober">checkout the readme</a> or open an issue.</p>
